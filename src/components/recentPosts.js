adfed4c350e62286c70d208218bcc8fb6a2bc01b
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-06-24 19:13:22 -0700

    added recent posts and search bar

---
+++ src/components/recentPosts.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="recent-posts">
+        <div className="recent-posts-wrapper">
+          <div className="recent-posts-heading">Recent Posts</div>
+          <ul className="recent-posts-posts">
+            <li>Recent Posts 0</li>
+            <li>Recent Posts 1</li>
+            <li>Recent Posts 2</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
