380618696724cae2ebe7fc1d99489d7fcc037ff0
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-06-25 13:25:36 -0700

    added api functionality, new logo and styles

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,31 @@
-import { combineReducers } from "redux";
+import { 
+  SET_RECENT_POSTS,
+  SET_RESULTS_POSTS
+} from './types';
 
-import { reducer as form } from "redux-form";
+import axios from 'axios';
 
-const rootReducer = combineReducers({
-  form
-});
+export function fetchRecentPosts() {
+  return function(dispatch) {
+      axios.get('https://api.dailysmarty.com/posts')
+          .then(response => {
+              dispatch({
+                  type: SET_RECENT_POSTS,
+                  payload: response.data.posts
+              })
+          })
+  }
+}
 
-export default rootReducer;
+export function fetchPostsWithQuery(query, callback) {
+  return function(dispatch) {
+      axios.get(`https://api.dailysmarty.com/search?q=${query}`)
+          .then(response => {
+              dispatch({
+                  type: SET_RESULTS_POSTS,
+                  payload: response.data.posts
+              })
+              if(callback) { callback() }
+          })
+  }
+}
