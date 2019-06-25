5cfcbab299613563ceec4c954e927086c6087e8d
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-06-24 22:07:53 -0700

    added exact route for home route and implemented with router HOC into the search bar to navigate user to results

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return (
-      <div className="recent-posts">
-        <div className="recent-posts-wrapper">
-          <div className="recent-posts-heading">Recent Posts</div>
-          <ul className="recent-posts-posts">
-            <li>Recent Posts 0</li>
-            <li>Recent Posts 1</li>
-            <li>Recent Posts 2</li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        <li>recent post 0</li>
+                        <li>recent post 1</li>
+                        <li>recent post 2</li>
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default SearchBar;
